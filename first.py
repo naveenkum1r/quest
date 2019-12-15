83575d5d6f35fe8ea448e38962e6f49c6e5f002e
naveen <naveen9958599096@gmail.com>
2019-12-15 17:03:42 +0530

    first lesson

---
+++ first.py
@@ -0,0 +1,39 @@
+spam_amount = 0
+print(spam_amount)
+
+spam_amount = spam_amount +4
+
+if spam_amount > 0 :
+    print("But I don't want any SPAM!")
+
+viking_song = "Spam " * spam_amount
+print(viking_song)
+
+type(spam_amount)
+type(19.95)
+print(5/2)
+print(6/2)
+print(5 //2)
+print(6//2)
+print(8-3+2)
+print(-3 + 4 * 2)
+
+hat_height_cm = 25
+my_height_cm = 190
+
+total_height_meters = hat_height_cm + my_height_cm / 100
+print("Height in meters = ", total_height_meters, "?")
+
+total_height_meters = (hat_height_cm + my_height_cm)/100
+print("Height in meters =",total_height_meters)
+
+print(min(1, 2, 3))
+print(max(1, 2, 3))
+
+print(abs(32))
+print(abs(-32))
+
+print(float(10))
+print(int(3.33))
+
+print(int('807') + 1)
