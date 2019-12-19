f8225c5bca9336726176b1591110935ab7996c3c
Naveen Kumar <naveen.kumar8@astrazeneca.com>
2019-12-19 23:34:54 +0530

    spliting data and finding out mean absolute error

--- machine_learning/fourth-2.py
+++ machine_learning/fourth-2.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import  mean_absolute_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 
-# Load data
 melbourne_file_path = '../input/melb_data.csv'
 melbourne_data = pd.read_csv(melbourne_file_path)
 # Filter rows with missing price values
@@ -11,9 +11,12 @@ filtered_melbourne_data = melbourne_data.dropna(axis=0)
 y = filtered_melbourne_data.Price
 melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 'YearBuilt', 'Lattitude', 'Longtitude']
 X = filtered_melbourne_data[melbourne_features]
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
 # Define model
 melbourne_model = DecisionTreeRegressor()
 # Fit model
-print(melbourne_model.fit(X, y))
-predicted_home_prices = melbourne_model.predict(X)
-print(mean_absolute_error(y, predicted_home_prices))
+melbourne_model.fit(train_X, train_y)
+
+# get predicted prices on validation data
+val_predictions = melbourne_model.predict(val_X)
+print(mean_absolute_error(val_y, val_predictions))
