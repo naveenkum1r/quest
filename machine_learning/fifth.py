6bd46ebe0a8f2a1b28257bb649c5eed7b6c37dc2
Naveen Kumar <naveen.kumar8@astrazeneca.com>
2019-12-20 21:59:19 +0530

    added lessons for overfitting and underfitting where this feature depends upon the max_leaf_nodes

---
+++ machine_learning/fifth.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+
+melbourne_file_path = '../input/melb_data.csv'
+melbourne_data = pd.read_csv(melbourne_file_path)
+# Filter rows with missing price values
+filtered_melbourne_data = melbourne_data.dropna(axis=0)
+# Choose target and features
+y = filtered_melbourne_data.Price
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 'YearBuilt', 'Lattitude', 'Longtitude']
+X = filtered_melbourne_data[melbourne_features]
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d \t\t Mean Absolute Error: %d" %(max_leaf_nodes, my_mae))
